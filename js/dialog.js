b195bd7f9b540f94ef6b80edb421294ad3616fa8
drMonro <rapman477@ya.ru>
2019-01-12 22:26:03 +0700

    #5.1 Модули

---
+++ js/dialog.js
@@ -0,0 +1,52 @@
+'use strict';
+
+(function () {
+  var setupFormElement = document.querySelector('.setup');
+  var dialogHandleElement = setupFormElement.querySelector('.upload');
+
+  dialogHandleElement.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shiftCoords = {
+        x: moveEvt.clientX - startCoords.x,
+        y: moveEvt.clientY - startCoords.y
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setupFormElement.style.top = (setupFormElement.offsetTop + shiftCoords.y) + 'px';
+      setupFormElement.style.left = (setupFormElement.offsetLeft + shiftCoords.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (dragEvt) {
+          dragEvt.preventDefault();
+          dialogHandleElement.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandleElement.addEventListener('click', onClickPreventDefault);
+      }
+    };
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+})();
